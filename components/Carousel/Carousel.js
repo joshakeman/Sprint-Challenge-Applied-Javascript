f5ac08ea0312c0ff2e4803b10901cc918ff6fbf4
Josh Akeman <joshakeman@gmail.com>
2019-03-08 15:19:32 -0500

    trying different apporach to carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,37 +1,18 @@
 class Carousel {
-    constructor(img, currentIndex) {
-        this.img = img;
-        // console.log(this.img);
-        this.currentIndex = currentIndex;
-        // console.log(this.currentIndex);
+    constructor(carousel) {
+        this.carousel = carousel;
+        console.log(this.carousel);
         this.imgList = document.querySelectorAll('.carousel img');
-        console.log(this.imgList);
-        
-        // this.currentImg = this.ImgList[currentIndex];
-        
-        if (this.currentIndex == 0) {
-            this.img.style.display = 'block';
-        }
-
-        this.rightButton = document.querySelector('.right-button');
-        this.rightButton.addEventListener('click', (event) => {
-            this.show();
-        });
+        console.log(this.imgList[0]);
     }
-    show () {
-        this.img.style.display = 'none';
-        if (currentIndex+1 != undefined) {
-            console.log(this.imgList[this.currentIndex+1]);
-        }
-    }
+    
 }
 
 class CarouselPic {
 
 }
 
-currentIndex = -1;
-let carousel = document.querySelectorAll('.carousel img').forEach( function(img) {
-    currentIndex ++
-    return new Carousel(img, currentIndex);
+
+let carousel = document.querySelectorAll('.carousel').forEach( function(carousel) {
+    return new Carousel(carousel);
 });
