ce272dd546c9a6505cfc96d284186a94fab5a3a1
Josh Akeman <joshakeman@gmail.com>
2019-03-08 15:43:57 -0500

    sliders starting to work

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,14 +3,34 @@ class Carousel {
         this.carousel = carousel;
         console.log(this.carousel);
         this.imgList = document.querySelectorAll('.carousel img');
-        console.log(this.imgList[0]);
+        console.log(this.imgList);
+        // this.imgList.forEach( function (img) {
+        //     return new CarouselPic;
+        // });
+        this.imgList[0].style.display = 'block';
+        this.currentIndex = 0; 
+
+        this.rightButton = document.querySelector('.right-button');
+        this.rightButton.addEventListener('click', (e) => {
+            this.clickRight();
+        });
     }
     
+    clickRight() {
+        this.imgList.forEach( function(img) {
+            img.style.display = 'none';
+        });
+        this.currentIndex ++
+        this.imgList[this.currentIndex].style.display = 'block';
+    }
 }
 
-class CarouselPic {
+// class CarouselPic {
+//     constructor(img) {
+//         this.img = img;
 
-}
+//     }
+// }
 
 
 let carousel = document.querySelectorAll('.carousel').forEach( function(carousel) {
