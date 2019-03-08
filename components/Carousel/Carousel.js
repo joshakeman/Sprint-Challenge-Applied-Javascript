a35e04bb67cd9a164135e4f824e86edf7d83e4de
Josh Akeman <joshakeman@gmail.com>
2019-03-08 13:52:09 -0500

    tried gettign carousel to work but failed.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,18 +1,37 @@
 class Carousel {
-    constructor(img) {
+    constructor(img, currentIndex) {
         this.img = img;
-        console.log(this.img);
-        this.imgList = document.querySelectorAll('.carouselImg');
+        // console.log(this.img);
+        this.currentIndex = currentIndex;
+        // console.log(this.currentIndex);
+        this.imgList = document.querySelectorAll('.carousel img');
         console.log(this.imgList);
         
+        // this.currentImg = this.ImgList[currentIndex];
         
-        
-    }
+        if (this.currentIndex == 0) {
+            this.img.style.display = 'block';
+        }
 
+        this.rightButton = document.querySelector('.right-button');
+        this.rightButton.addEventListener('click', (event) => {
+            this.show();
+        });
+    }
+    show () {
+        this.img.style.display = 'none';
+        if (currentIndex+1 != undefined) {
+            console.log(this.imgList[this.currentIndex+1]);
+        }
+    }
+}
+
+class CarouselPic {
 
 }
 
-
-let carousel = document.querySelectorAll('.carouselImg').forEach( function(img) {
-    return new Carousel(img);
+currentIndex = -1;
+let carousel = document.querySelectorAll('.carousel img').forEach( function(img) {
+    currentIndex ++
+    return new Carousel(img, currentIndex);
 });
